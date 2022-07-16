b57aed087e25b80c8eb079e990342d5650b4f9cb
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 12:09:31 +0800

    java11

--- src/main/java/com/ocp/day03/ArrayDemo5.java
+++ src/main/java/com/ocp/day03/ArrayDemo5.java
@@ -7,8 +7,11 @@ public class ArrayDemo5 {
         // 建立陣列 + {初始值...}
         int[] scores = {95, 45, 37, 81, 75};
         System.out.println(Arrays.toString(scores));
-        // 印出及格的分數的平均
+        // 印出及格的分數
         // 使用 Java stream
-        Arrays.stream(scores).forEach(score -> System.out.println(score));
+        Arrays.stream(scores)
+              .filter(score -> score >= 60)
+              .forEach(score -> System.out.println(score));
+        
     }
 }
