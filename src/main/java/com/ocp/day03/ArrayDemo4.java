54df93b21f9273e6c5eefbefb87f7c5295b52137
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 11:57:49 +0800

    java11

--- src/main/java/com/ocp/day03/ArrayDemo4.java
+++ src/main/java/com/ocp/day03/ArrayDemo4.java
@@ -8,7 +8,7 @@ public class ArrayDemo4 {
         //int[] scores = {95, 45, 37, 81, 75};
         int[] scores = new int[]{95, 45, 37, 81, 75}; //  有設定初始值時, 不可再定義陣列長度
         System.out.println(Arrays.toString(scores));
-        // 印出及格的分數的總平均
+        // 印出及格的分數的平均
         int sum = 0; // 及格分數的總分
         int pass_length = 0; // 及格分數的元素個數
         for(int i=0;i<scores.length;i++) {
---
+++ src/main/java/com/ocp/day03/ArrayDemo5.java
@@ -0,0 +1,12 @@
+package com.ocp.day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo5 {
+    public static void main(String[] args) {
+        // 建立陣列 + {初始值...}
+        int[] scores = {95, 45, 37, 81, 75};
+        System.out.println(Arrays.toString(scores));
+        // 印出及格的分數的平均
+    }
+}
