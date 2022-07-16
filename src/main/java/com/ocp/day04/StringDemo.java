acd8126ed4c3a74e44abd14638b166f34ff5fb99
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 15:54:30 +0800

    java11

--- src/main/java/com/ocp/day04/StringDemo.java
+++ src/main/java/com/ocp/day04/StringDemo.java
@@ -6,8 +6,8 @@ public class StringDemo {
         String s2 = new String("Java");
         String s3 = new String("jAVA");
         System.out.println(s1 == s2);  // false
-        System.out.println(s1.equals(s2));  // true
+        System.out.println(s1.equals(s2));  // true, equals() 大小寫視為不同
         System.out.println(s1.equals(s3));  // false
-        System.out.println(s1.equalsIgnoreCase(s3));  // true
+        System.out.println(s1.equalsIgnoreCase(s3));  // true, equalsIgnoreCase 大小寫視為相同
     }
 }
