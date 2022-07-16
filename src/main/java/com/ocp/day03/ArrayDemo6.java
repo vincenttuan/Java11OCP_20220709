5fed6a9c96ada42f590c00af7233641c56849135
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 12:10:19 +0800

    java11

---
+++ src/main/java/com/ocp/day03/ArrayDemo6.java
@@ -0,0 +1,14 @@
+package com.ocp.day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo6 {
+    public static void main(String[] args) {
+        // 建立陣列 + {初始值...}
+        int[] scores = {95, 45, 37, 81, 75};
+        System.out.println(Arrays.toString(scores));
+        // 印出及格的分數的平均
+        // 使用 Java stream
+        
+    }
+}
