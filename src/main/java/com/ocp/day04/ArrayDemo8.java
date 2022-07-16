6584cbd8b326debd6614c7d8b8eed81c93145238
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 14:14:30 +0800

    java11

---
+++ src/main/java/com/ocp/day04/ArrayDemo8.java
@@ -0,0 +1,12 @@
+package com.ocp.day04;
+
+public class ArrayDemo8 {
+    public static void main(String[] args) {
+        // 字串陣列
+        String[] names = {"Java", "Python", "Go", "Foxpro", "Objective-C", "Swift", "Kotlin", "C"};
+        System.out.println("陣列長度: " + names.length);  // 陣列元素個數
+        // 字串
+        String name = "Foxpro2.5";
+        System.out.println("字串長度: " + name.length());  // 字串字元個數
+    }
+}
