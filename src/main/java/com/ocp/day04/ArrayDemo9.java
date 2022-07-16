d0a385f0243dcefe70fb757cb67510b689b5772f
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 14:29:49 +0800

    java11

--- src/main/java/com/ocp/day04/ArrayDemo9.java
+++ src/main/java/com/ocp/day04/ArrayDemo9.java
@@ -5,14 +5,14 @@ import java.util.Arrays;
 public class ArrayDemo9 {
     public static void main(String[] args) {
         // 字串陣列
-        String[] names = {"Java", "Python", "Go", "Foxpro", "Objective-C", "Swift", "Kotlin", "C"};
+        String[] names = {"Java", "Python", "Go", null, "Foxpro", "Objective-C", "Swift", "Kotlin", "C"};
         //String[] names = {"Java", "Go"};
         System.out.println(Arrays.toString(names));
         // 請求出平均字串長度為何?
         int sum = 0; // 字串元素總長度
         for(int i=0;i<names.length;i++) {
             String name = names[i];  // 取得字串元素
-            int lens = name.length(); // 取得字串元素的長度
+            int lens = (name == null) ? 0 : name.length(); // 取得字串元素的長度
             sum += lens;
             // log
             System.out.printf("%s length: %d\n", name, lens);
