e3ec954fa16aa95e074a57b4ef210743855270cb
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 11:34:22 +0800

    java11

--- src/main/java/com/ocp/day03/ArrayDemo2.java
+++ src/main/java/com/ocp/day03/ArrayDemo2.java
@@ -6,6 +6,9 @@ public class ArrayDemo2 {
     public static void main(String[] args) {
         int[] scores = new int[3];  // 陣列長度一經過宣告之後就固定了
         System.out.println(Arrays.toString(scores));
+        
+        //System.out.println("scores[3]: " + scores[3]);
+        
         // 陣列的最小維度 = 0
         // 陣列的最大維度 = 陣列的長度 - 1
         int maxLength = scores.length - 1;
