f5de685ed357465b7faae8d31d46daac154b872b
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 10:50:31 +0800

    java11

---
+++ src/main/java/com/ocp/day03/ArrayDemo3.java
@@ -0,0 +1,21 @@
+package com.ocp.day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo3 {
+    public static void main(String[] args) {
+        int[] scores = new int[3];
+        System.out.println(Arrays.toString(scores));
+        // 將內容放入到元素中
+        scores[0] = 100;
+        scores[1] = 85;
+        scores[2] = 68;
+        System.out.println(Arrays.toString(scores));
+        // 計算總分 I (使用 for-loop)
+        int sum = 0;
+        for(int i = 0 ; i < scores.length ; i++) {
+            sum += scores[i];
+        }
+        System.out.println("總分: " + sum);
+    }
+}
