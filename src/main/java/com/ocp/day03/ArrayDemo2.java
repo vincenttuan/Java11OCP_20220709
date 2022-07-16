920e727a16cee9b4eb5b396fc7e2ef09b319efe0
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-16 10:35:46 +0800

    java11

---
+++ src/main/java/com/ocp/day03/ArrayDemo2.java
@@ -0,0 +1,20 @@
+package com.ocp.day03;
+
+import java.util.Arrays;
+
+public class ArrayDemo2 {
+    public static void main(String[] args) {
+        int[] scores = new int[3];
+        System.out.println(Arrays.toString(scores));
+        // 陣列的最小維度 = 0
+        // 陣列的最大維度 = 陣列的長度 - 1
+        int maxLength = scores.length - 1;
+        System.out.println("陣列的最大維度: " + maxLength);
+        if (maxLength >= 3) {
+            System.out.println("scores[3]: " + scores[3]);
+        }
+        if (maxLength >= 2) {
+            System.out.println("scores[2]: " + scores[2]);
+        }
+    }
+}
