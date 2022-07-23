06242d6273fae24d9318b995c5b9f930c8b85b47
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 10:09:19 +0800

    java11

--- src/main/java/com/ocp/day05/MultiArrayDemo4.java
+++ src/main/java/com/ocp/day05/MultiArrayDemo4.java
@@ -6,6 +6,12 @@ public class MultiArrayDemo4 {
     public static void main(String[] args) {
         // 多維陣列的初始值
         int[][] m = {{100, 90, 80}, {70, 60, 50}}; // [2]x[3] 的二維陣列(對稱型)
-        
+        // 將資料透過 for-loop 依序印出
+        for(int i=0 ; i < m.length ; i++) {
+            System.out.println(m[i]);
+            for(int k=0 ; k < m[i].length ; k++) {
+                System.out.printf("m[%d][%d]: %d\n", i, k, m[i][k]);
+            }
+        }
     }
 }
