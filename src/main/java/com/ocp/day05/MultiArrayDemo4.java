8cd59d814423ccb847db0c33ca691ccfd4e6f3ca
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 10:06:01 +0800

    java11

---
+++ src/main/java/com/ocp/day05/MultiArrayDemo4.java
@@ -0,0 +1,11 @@
+package com.ocp.day05;
+
+import java.util.Arrays;
+
+public class MultiArrayDemo4 {
+    public static void main(String[] args) {
+        // 多維陣列的初始值
+        int[][] m = {{100, 90, 80}, {70, 60, 50}}; // [2]x[3] 的二維陣列(對稱型)
+        
+    }
+}
