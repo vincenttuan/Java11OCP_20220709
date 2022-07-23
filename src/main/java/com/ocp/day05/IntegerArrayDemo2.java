43180c56d09fdbad49f29d803eb21bdb4f2663c3
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 12:17:30 +0800

    java11

---
+++ src/main/java/com/ocp/day05/IntegerArrayDemo2.java
@@ -0,0 +1,9 @@
+package com.ocp.day05;
+
+public class IntegerArrayDemo2 {
+    public static void main(String[] args) {
+        Integer[][] scores = {{100, 90, null}, {80, null, 70}};
+        // null 表示未參與考試, 請求出有考試的平均分
+        
+    }
+}
