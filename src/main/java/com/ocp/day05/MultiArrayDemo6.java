bfc391c70b718da2c7292a0270febba69f5da156
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 10:50:24 +0800

    java11

---
+++ src/main/java/com/ocp/day05/MultiArrayDemo6.java
@@ -0,0 +1,10 @@
+package com.ocp.day05;
+
+public class MultiArrayDemo6 {
+    public static void main(String[] args) {
+        // 多維陣列的初始值
+        int[][] m = {{100, 90, 80}, {70, 60, 50}, {40, 30, 20}};
+        // 請計算出元素個數, 總和, 平均, 最大, 最小
+        
+    }
+}
