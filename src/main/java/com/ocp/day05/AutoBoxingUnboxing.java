64a2639c96c7fb416439f81881a01562e45b8097
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 11:48:55 +0800

    java11

---
+++ src/main/java/com/ocp/day05/AutoBoxingUnboxing.java
@@ -0,0 +1,17 @@
+package com.ocp.day05;
+
+public class AutoBoxingUnboxing {
+    public static void main(String[] args) {
+        // Java 5 以前
+        Integer num1 = Integer.valueOf(100); // 建立一個整數物件
+        int x1 = num1.intValue(); // 透過 intValue() 將資料取出
+        System.out.println(x1); 
+        
+        // Java 5 提供了自動拆箱(unboxing), 裝箱(boxing)功能
+        Integer num2 = 100; // 編譯器會自動轉為 Integer.valueOf(100), auto-boxing 自動裝箱
+        int x2 = num2; // 編譯器會自動轉為 num2.intValue(), auto-unboxing 自動拆箱
+        System.out.println(x2); 
+        
+        
+    }
+}
