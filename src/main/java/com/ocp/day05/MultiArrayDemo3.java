18a3b957fafaac6ad5a6cf9d66419b10c66f3e99
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-23 10:05:25 +0800

    java11

--- src/main/java/com/ocp/day05/MultiArrayDemo3.java
+++ src/main/java/com/ocp/day05/MultiArrayDemo3.java
@@ -6,9 +6,9 @@ public class MultiArrayDemo3 {
     public static void main(String[] args) {
         // 多維陣列的初始值
         int[][] m = {{100, 90, 80}, {70, 60, 50}}; // [2]x[3] 的二維陣列(對稱型)
-        System.out.println("m: " + m);
-        System.out.println("m[0]: " + m[0]);
-        System.out.println("m[1]: " + m[1]);
+        System.out.println("m: " + m + ", length: " + m.length);
+        System.out.println("m[0]: " + m[0] + ", length: " + m[0].length);
+        System.out.println("m[1]: " + m[1] + ", length: " + m[1].length);
         
         System.out.println("m[0][0]: " + m[0][0]);  // 內容值
         System.out.println("m[0][1]: " + m[0][1]);
