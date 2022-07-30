0974b596ee9506e7763250aa3c5ec05c882a1e61
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 16:18:12 +0800

    java11

--- src/main/java/com/ocp/computer/RAM.java
+++ src/main/java/com/ocp/computer/RAM.java
@@ -1,5 +1,9 @@
 package com.ocp.computer;
 
 public class RAM extends Hardware {
-    
+    public Integer capacity;
+    public RAM() {
+        capacity = 32; // GB
+        price = 3000;
+    }
 }
--- src/main/java/com/ocp/computer/SSD.java
+++ src/main/java/com/ocp/computer/SSD.java
@@ -1,5 +1,9 @@
 package com.ocp.computer;
 
 public class SSD extends Hardware {
-    
+    public Integer size;
+    public SSD() {
+        size = 512; // GB
+        price = 4500;
+    }
 }
