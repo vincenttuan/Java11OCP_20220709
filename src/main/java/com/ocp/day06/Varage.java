ee981b75eb818a4266ac2370a57c3ebd6d883b05
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 15:03:41 +0800

    java11

--- src/main/java/com/ocp/day06/Varage.java
+++ src/main/java/com/ocp/day06/Varage.java
@@ -1,13 +1,17 @@
 package com.ocp.day06;
 
 public class Varage {
-    public static void call(int x) {
-        System.out.println("A");
+    public static void call(int... x) {
+        System.out.println("C");
     }
     public static void call(Integer x, Integer y) {
         System.out.println("B");
     }
-    public static void call(int... x) {
-        System.out.println("C");
+    public static void call(int x, int y) {
+        System.out.println("BB");
     }
+    public static void call(int x) {
+        System.out.println("A");
+    }
+    
 }
---
+++ src/test/java/com/ocp/day06/VarageTest.java
@@ -0,0 +1,9 @@
+package com.ocp.day06;
+
+public class VarageTest {
+    public static void main(String[] args) {
+        Varage.call(10);
+        Varage.call(10, 20);
+        Varage.call(10, 20, 30);
+    }
+}
