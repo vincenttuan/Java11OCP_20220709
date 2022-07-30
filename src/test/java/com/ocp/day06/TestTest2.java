7801f2dbeda95e2fd13c6b1a4298acf5c9e60adb
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 12:00:32 +0800

    java11

--- src/test/java/com/ocp/day06/TestTest2.java
+++ src/test/java/com/ocp/day06/TestTest2.java
@@ -1,5 +1,6 @@
 package com.ocp.day06;
 
+import java.util.OptionalDouble;
 import java.util.stream.Stream;
 
 public class TestTest2 {
@@ -7,6 +8,7 @@ public class TestTest2 {
         Test t1= new Test("國文", 100);
         Test t2= new Test("英文", 50);
         Test[] tests = {t1, t2, new Test("數學", 75)};
+        //Test[] tests = {};
         System.out.println(tests);
         // for-loop 走訪資料
         for(int i=0;i<tests.length;i++) {
@@ -19,5 +21,16 @@ public class TestTest2 {
         // Java 8 Stream
         Stream.of(tests).forEach(test -> System.out.println(test));
         Stream.of(tests).forEach(System.out::println);
+        // 總分與平均
+        int sum = Stream.of(tests).mapToInt(test -> test.getScore()).sum();
+        int sum2 = Stream.of(tests).mapToInt(Test::getScore).sum();
+        System.out.println(sum + ", " + sum2);
+        OptionalDouble optAvg = Stream.of(tests).mapToInt(Test::getScore).average();
+        if(optAvg.isEmpty()) {
+            System.out.println("無法計算平均");
+        } else {
+            System.out.println("平均: " + optAvg.getAsDouble());
+        }
+        
     }
 }
