773d6752535a000079ee338e3bd8a10afb6397f1
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 16:19:26 +0800

    java11

--- src/main/java/com/ocp/computer/Machine.java
+++ src/main/java/com/ocp/computer/Machine.java
@@ -1,5 +1,10 @@
 package com.ocp.computer;
 
 public class Machine extends Hardware {
+    public Integer power;
     
+    public Machine() {
+        power = 650; // 瓦
+        price = 2800;
+    }
 }
