d144cb8fc4b1de321b8b86a050cc85401dd07ebc
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 15:51:49 +0800

    java11

---
+++ src/main/java/com/ocp/day06/Base.java
@@ -0,0 +1,8 @@
+package com.ocp.day06;
+
+public class Base {
+    public Base() {
+        System.out.println("Base");
+    }
+}
+
---
+++ src/main/java/com/ocp/day06/Sub.java
@@ -0,0 +1,7 @@
+package com.ocp.day06;
+
+public class Sub extends Base {
+    public Sub() {
+        System.out.println("Sub");
+    }
+}
