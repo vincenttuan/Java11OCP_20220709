a9267a31341c17c6fca6674ab5fd1d6190311100
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 14:56:28 +0800

    java11

--- src/main/java/com/ocp/day06/CalcUtil.java
+++ src/main/java/com/ocp/day06/CalcUtil.java
@@ -2,15 +2,8 @@ package com.ocp.day06;
 
 // 計算工具
 public class CalcUtil {
-    public static int add(int x, int y) {
-        return x + y;
-    }
-    
-    public static int add(int x, int y, int z) {
-        return x + y + z;
-    }
-    
-    public static int add(int [] nums) {
+    // ... 表示可以放置 0~N 個參數(也可以放陣列參數)
+    public static int add(int... nums) {
         int sum = 0;
         for(int n : nums) {
             sum += n;
--- src/test/java/com/ocp/day06/CalcUtilTest.java
+++ src/test/java/com/ocp/day06/CalcUtilTest.java
@@ -6,6 +6,9 @@ public class CalcUtilTest {
         System.out.println(CalcUtil.add(10, 20, 30));
         int[] nums = {10, 20, 30, 40, 50};
         System.out.println(CalcUtil.add(nums));
+        System.out.println(CalcUtil.add(50));
+        System.out.println(CalcUtil.add()); // 不放置參數也行
+
         System.out.println(CalcUtil.sub(10, 20));
     }
 }
