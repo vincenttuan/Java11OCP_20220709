13036a8144f16955e4ec1dc12e846168ba620868
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 10:20:28 +0800

    java11

--- src/test/java/com/ocp/day06/ExamTest.java
+++ src/test/java/com/ocp/day06/ExamTest.java
@@ -4,7 +4,7 @@ public class ExamTest {
     public static void main(String[] args) {
         Exam exam = new Exam();
         exam.subjectName = "國文";
-        exam.setScore(90);
+        exam.setScore(-90);
         System.out.printf("國文: %s\n", exam.subjectName);
         System.out.printf("分數: %d\n", exam.getScore());
         System.out.printf("及格: %b\n", exam.isPass());
