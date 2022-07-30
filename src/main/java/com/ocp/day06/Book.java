5d72c06f03004e4c015a783e187edd0e1db7f028
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 14:36:51 +0800

    java11

---
+++ src/main/java/com/ocp/day06/Book.java
@@ -0,0 +1,10 @@
+package com.ocp.day06;
+
+public class Book {
+    int price = 100;
+    // 自己寫建構子
+    public Book() {
+        price = 200;
+    }
+    
+}
---
+++ src/test/java/com/ocp/day06/BookTest.java
@@ -0,0 +1,8 @@
+package com.ocp.day06;
+
+public class BookTest {
+    public static void main(String[] args) {
+        Book book = new Book();
+        System.out.println(book.price);
+    }
+}
