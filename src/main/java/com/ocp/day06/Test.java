6dc4385380b8e940cb6843bd8ae6b535b1f89da0
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 10:26:59 +0800

    java11

---
+++ src/main/java/com/ocp/day06/Test.java
@@ -0,0 +1,17 @@
+package com.ocp.day06;
+
+// 隨堂測驗
+public class Test {
+    String subjectName; // 科目
+    private Integer score; // 分數
+    
+    public Integer getScore() {
+        return score;
+    }
+
+    public void setScore(Integer score) {
+        if(score >= 0 && score <= 100) {
+            this.score = score;
+        }
+    }
+}
---
+++ src/test/java/com/ocp/day06/TestTest.java
@@ -0,0 +1,12 @@
+package com.ocp.day06;
+
+public class TestTest {
+    public static void main(String[] args) {
+        Test test = new Test();
+        test.subjectName = "國文";
+        test.setScore(90);
+        System.out.printf("科目: %s\n", test.subjectName);
+        System.out.printf("分數: %d\n", test.getScore());
+        
+    }
+}
