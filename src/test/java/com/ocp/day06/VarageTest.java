4fe2ac5dd9ba6775f30b6450a62b292078deddc1
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 15:04:44 +0800

    java11

--- src/test/java/com/ocp/day06/VarageTest.java
+++ src/test/java/com/ocp/day06/VarageTest.java
@@ -5,5 +5,6 @@ public class VarageTest {
         Varage.call(10);
         Varage.call(10, 20);
         Varage.call(10, 20, 30);
+        Varage.call(Integer.valueOf(10), Integer.valueOf(20));
     }
 }
