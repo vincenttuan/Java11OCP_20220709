400e7b41079597902298f7009fbe9a769dfcc77b
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 14:05:52 +0800

    java11

---
+++ src/main/java/com/ocp/day06/CalcUtil.java
@@ -0,0 +1,11 @@
+package com.ocp.day06;
+
+// 計算工具
+public class CalcUtil {
+    public static int add(int x, int y) {
+        return x + y;
+    }
+    public static int sub(int x, int y) {
+        return x - y;
+    }
+}
---
+++ src/test/java/com/ocp/day06/CalcUtilTest.java
@@ -0,0 +1,8 @@
+package com.ocp.day06;
+
+public class CalcUtilTest {
+    public static void main(String[] args) {
+        System.out.println(CalcUtil.add(10, 20));
+        System.out.println(CalcUtil.sub(10, 20));
+    }
+}
