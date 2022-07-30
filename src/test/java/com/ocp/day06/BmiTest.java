bfdd4a2825f6b1baa1ecd0646ee97da94984a6bb
MB-teacher <MB-teacher@MB-207-teacher>
2022-07-30 10:00:22 +0800

    java11

--- src/test/java/com/ocp/day06/BmiTest.java
+++ src/test/java/com/ocp/day06/BmiTest.java
@@ -7,5 +7,11 @@ public class BmiTest {
         bmi.weight = 60.0;
         double bmiValue = bmi.calcAndGetBmi();
         System.out.printf("%.2f\n", bmiValue);
+        //-------------------------------------
+        Bmi bmi2 = new Bmi();
+        bmi2.height = 165;
+        bmi2.weight = 55;
+        double bmiValue2 = bmi2.calcAndGetBmi();
+        System.out.printf("%.2f\n", bmiValue2);
     }
 }
