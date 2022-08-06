96935b70cc05e22b610899ce922b660ba3314e7e
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-06 13:52:57 +0800

    java11

--- src/main/java/com/ocp/day08/Bottle.java
+++ src/main/java/com/ocp/day08/Bottle.java
@@ -1,6 +1,16 @@
 package com.ocp.day08;
 
 // 寶特瓶
-public class Bottle {
+public class Bottle implements Container {
+
+    @Override
+    public void printName() {
+        System.out.println("寶特瓶");
+    }
+
+    @Override
+    public int capacity() {
+        return 250;
+    }
     
 }
--- src/main/java/com/ocp/day08/PoilPack.java
+++ src/main/java/com/ocp/day08/PoilPack.java
@@ -1,6 +1,16 @@
 package com.ocp.day08;
 
 // 鋁箔包
-public class PoilPack {
+public class PoilPack implements Container {
+
+    @Override
+    public void printName() {
+        System.out.println("鋁箔包");
+    }
+
+    @Override
+    public int capacity() {
+        return 250;
+    }
     
 }
