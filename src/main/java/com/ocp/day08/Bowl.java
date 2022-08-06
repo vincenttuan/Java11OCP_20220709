975bd6322375e6e5b4002e9cf121f1170e0e0ed9
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-06 14:27:18 +0800

    java11

---
+++ src/main/java/com/ocp/day08/Bowl.java
@@ -0,0 +1,10 @@
+package com.ocp.day08;
+
+public class Bowl extends AContainer {
+
+    @Override
+    public void printName() {
+        System.out.println("碗");
+    }
+    
+}
---
+++ src/main/java/com/ocp/day08/OilBottle.java
@@ -0,0 +1,10 @@
+package com.ocp.day08;
+
+public class OilBottle extends BigAContainer {
+
+    @Override
+    public void printName() {
+        System.out.println("油瓶");
+    }
+    
+}
