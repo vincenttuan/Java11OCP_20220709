6b46cd7564cbd22cd5204fe109020035cabb218d
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-06 10:37:13 +0800

    java11

--- src/main/java/com/ocp/day07/Hunter.java
+++ src/main/java/com/ocp/day07/Hunter.java
@@ -1,5 +1,11 @@
 package com.ocp.day07;
 
 public class Hunter extends Actor {
+
+    @Override
+    public void attack() {
+        System.out.print(getClass().getSimpleName() + " ");
+        System.out.println("用槍射擊");
+    }
     
 }
--- src/main/java/com/ocp/day07/Magician.java
+++ src/main/java/com/ocp/day07/Magician.java
@@ -1,5 +1,11 @@
 package com.ocp.day07;
 
 public class Magician extends Actor {
+
+    @Override
+    public void attack() {
+        System.out.print(getClass().getSimpleName() + " ");
+        System.out.println("施展魔法攻擊");
+    }
     
 }
