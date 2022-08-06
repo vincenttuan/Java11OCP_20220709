f2ed5f27768e71fec08a067e99c1e4a7d436c105
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-06 09:49:39 +0800

    java11

---
+++ src/main/java/com/ocp/day07/OverrideMethod.java
@@ -0,0 +1,22 @@
+package com.ocp.day07;
+
+class Father {
+    Animal play(int x, String s) { return null; }
+}
+
+class Son extends Father {
+    @Override
+    Dog play(int x, String s) { return null; }
+}
+
+class Animal {
+
+}
+
+class Dog extends Animal {
+
+}
+
+public class OverrideMethod {
+    
+}
