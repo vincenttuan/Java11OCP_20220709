88fce9e01345ed4a10e0765bcdc5f62ec1d72c89
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-06 14:36:32 +0800

    java11

--- src/test/java/com/ocp/day08/ContainerTest2.java
+++ src/test/java/com/ocp/day08/ContainerTest2.java
@@ -5,6 +5,16 @@ public class ContainerTest2 {
         Bottle bottle = new Bottle(); // 寶特瓶
         Bowl bowl = new Bowl(); // 碗
         FoilPack foilPack = new FoilPack(); // 鋁箔包
+        BigBottle bigBottle = new BigBottle(); // 重量級寶特瓶
+        OilBottle oilBottle = new OilBottle(); // 油瓶
+        
+        // 所有容器
+        Container[] containers = {bottle, bowl, foilPack, bigBottle, oilBottle};
+        // 所有小容器
+        AContainer[] aContainers = {bottle, bowl, foilPack};
+        // 所有大容器
+        BigAContainer[] bigAContainer = {bigBottle, oilBottle};
+        
         
     }
 }
