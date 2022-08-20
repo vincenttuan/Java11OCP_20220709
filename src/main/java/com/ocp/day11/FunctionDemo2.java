8a22653c636575be5611b335c5a066fc26301d9b
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 15:21:25 +0800

    java11

--- src/main/java/com/ocp/day11/FunctionDemo2.java
+++ src/main/java/com/ocp/day11/FunctionDemo2.java
@@ -1,13 +1,20 @@
 package com.ocp.day11;
 
 import java.util.function.BiConsumer;
+import java.util.function.ObjIntConsumer;
 
 public class FunctionDemo2 {
     public static void main(String[] args) {
         // BiConsumer<T, U>
+        // void accept(T t, U u)
         BiConsumer<String, Integer> biConsumer = 
                 (name, cost) -> System.out.printf("品名: %s 成本:$%d 價格:$%d\n", name, cost, cost*15);
         biConsumer.accept("冬瓜茶", 2);
         
+        // ObjIntConsumer<T>
+        // void accept(T t, int value)
+        ObjIntConsumer<String> objIntConsumer = 
+                (name, cost) -> System.out.printf("品名: %s 成本:$%d 價格:$%d\n", name, cost, cost*15);
+        objIntConsumer.accept("冬瓜茶", 2);
     }
 }
