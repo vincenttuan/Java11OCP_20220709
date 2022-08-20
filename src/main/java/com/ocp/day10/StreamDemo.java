270ecc98c5c77839ed7e4a8df9f1d89c36d21077
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 16:34:44 +0800

    java11

--- src/main/java/com/ocp/day10/StreamDemo.java
+++ src/main/java/com/ocp/day10/StreamDemo.java
@@ -13,7 +13,7 @@ public class StreamDemo {
               .forEach(name -> System.out.println(name));
         // 在名字 4 個字以上的成員中選一個出來當班長(leader)
         String leader = Arrays.stream(names) // 將陣列串流化
-                              .parallel() // 平行處理
+                              .parallel() // 平行處理, (預設是 .sequential() 循序處理)
                               .filter(name -> name.length() > 4)
                               .findAny()  // 任意抓取一人
                               .get(); // 取出人名
