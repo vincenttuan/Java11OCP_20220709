08804a2cf87d65b6224133b5e461b2f03c2bb7f6
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 12:03:30 +0800

    java11

--- src/main/java/com/ocp/day10/Anonymouse2.java
+++ src/main/java/com/ocp/day10/Anonymouse2.java
@@ -9,12 +9,15 @@ public class Anonymouse2 {
                 return x + y;
             }
         };
-        // Lambda 語法 1
+        // Lambda 語法 1 (標準版)
         Calculator calculator2 = (double x, double y) -> {
             return x + y;
         };
+        // Lambda 語法 2 (簡化版)
+        Calculator calculator3 = (x, y) -> x + y;
         
         System.out.println(calculator.calc(10, 20));
         System.out.println(calculator2.calc(10, 20));
+        System.out.println(calculator3.calc(10, 20));
     }
 }
