857f063ff2b0bc55c7750924988079d548245d2b
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 11:41:04 +0800

    java11

--- src/main/java/com/ocp/day10/Anonymouse.java
+++ src/main/java/com/ocp/day10/Anonymouse.java
@@ -1,5 +1,19 @@
 package com.ocp.day10;
 
 public class Anonymouse {
-    
+    public static void main(String[] args) {
+        // 介面不可以直接 new
+        // Calculator calculator = new Calculator();
+        
+        // 介面不可以直接 new 的原因是因為介面的方法沒有實作
+        // 所以可以利用匿名內部類別的技術{}來實作方法
+        // 這樣就可以 new
+        Calculator calculator = new Calculator(){
+            public double calc(double x, double y) {
+                return x + y;
+            }
+        };
+        System.out.println(calculator.calc(10, 20));
+        
+    }
 }
