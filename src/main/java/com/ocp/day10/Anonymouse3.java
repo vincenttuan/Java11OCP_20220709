56e119cae3276fc2d6b6c0f6b9f11b8453d591e2
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 12:16:40 +0800

    java11

--- src/main/java/com/ocp/day10/Anonymouse3.java
+++ src/main/java/com/ocp/day10/Anonymouse3.java
@@ -2,6 +2,20 @@ package com.ocp.day10;
 
 public class Anonymouse3 {
     public static void main(String[] args) {
+        // Lambda (標準版)
+        Printer printer1 = (String data) -> {
+            System.out.println(data);
+        };
+        printer1.print("中午吃啥?");
+        
+        // Lambda (精簡版)
+        Printer printer2 = (data) -> System.out.println(data);
+        printer2.print("晚上吃啥?");
+        
+        // Lambda (精簡版 + :: 方法參考)
+        Printer printer3 = System.out::println;
+        printer3.print("消夜吃啥?");
+        
         
     }
 }
