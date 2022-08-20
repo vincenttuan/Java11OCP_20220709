8ef01b0b5a762aa48aaa3aafb53c3f2b96d33ee2
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-20 10:51:31 +0800

    java11

---
+++ src/test/java/com/ocp/day10/OuterTest4.java
@@ -0,0 +1,10 @@
+package com.ocp.day10;
+
+public class OuterTest4 {
+    public static void main(String[] args) {
+        // 建立 Outer 物件
+        Outer outer = new Outer();
+        // 呼叫 foo() 方法
+        outer.foo();
+    }
+}
