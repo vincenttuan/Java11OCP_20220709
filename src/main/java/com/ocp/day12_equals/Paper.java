87b05cf3b8a8541cd9d79573250db5a058bd1e23
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-27 16:05:44 +0800

    java11

--- src/main/java/com/ocp/day12_equals/Paper.java
+++ src/main/java/com/ocp/day12_equals/Paper.java
@@ -1,5 +1,7 @@
 package com.ocp.day12_equals;
 
+import java.util.Objects;
+
 enum Size {
     A4, B5, A3, B4
 }
@@ -21,7 +23,48 @@ public class Paper {
         this.color = color;
         this.price = price;
     }
+    
+    // 使用精靈產生 hashCode() 與 equals() 的實作
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 97 * hash + Objects.hashCode(this.brandName);
+        hash = 97 * hash + this.pound;
+        hash = 97 * hash + Objects.hashCode(this.size);
+        hash = 97 * hash + Objects.hashCode(this.color);
+        hash = 97 * hash + this.price;
+        return hash;
+    }
 
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Paper other = (Paper) obj;
+        if (this.pound != other.pound) {
+            return false;
+        }
+        if (this.price != other.price) {
+            return false;
+        }
+        if (!Objects.equals(this.brandName, other.brandName)) {
+            return false;
+        }
+        if (this.size != other.size) {
+            return false;
+        }
+        return this.color == other.color;
+    }
+    
+    
+    
     @Override
     public String toString() {
         return "Paper{" + "brandName=" + brandName + ", pound=" + pound + ", size=" + size + ", color=" + color + ", price=" + price + '}';
