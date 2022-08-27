33f798ce0efae16bdcaa1ab6e73f3ada641abcdb
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-27 11:03:36 +0800

    java11

---
+++ src/main/java/com/ocp/solid/ocp/before/EditTest.java
@@ -0,0 +1,27 @@
+package com.ocp.solid.ocp.before;
+
+class Rectangle {
+    public String toString() { return "畫矩形";};
+}
+
+class Traingle {
+    public String toString() { return "畫三角形";};
+}
+
+class Edit { // 編輯板 (不可改變)
+    public void draw(Rectangle rectangle) {
+        System.out.println(rectangle);
+    }
+    public void draw(Traingle traingle) {
+        System.out.println(traingle);
+    }
+}
+
+public class EditTest {
+    public static void main(String[] args) {
+        // 使用 Edit 物件
+        Edit edit = new Edit();
+        edit.draw(new Rectangle());
+        edit.draw(new Traingle());
+    }
+}
