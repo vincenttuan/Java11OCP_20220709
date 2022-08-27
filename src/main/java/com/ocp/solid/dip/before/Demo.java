b031dbfb330d4e4fc2e6b46b499e514d2c0cc501
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-27 10:11:44 +0800

    java11

--- src/main/java/com/ocp/solid/dip/before/Demo.java
+++ src/main/java/com/ocp/solid/dip/before/Demo.java
@@ -8,19 +8,28 @@ class Line { // 功能服務 二
     public String message() { return "line...";}
 }
 
-class MyPhone { // 手機
+class SMS { //  功能服務 三 (額外加上)
+    public String message() { return "sms...";}
+}
+
+class MyPhone { // 手機實體(不可再變動了)
     public void read(Email email) { // 讀取 Email 服務訊息
         System.out.println(email.message());
     }
     public void read(Line line) { // 讀取 Line 服務訊息
         System.out.println(line.message());
     }
+    // 被迫加入實作, 違反"開放關閉原則"
+    public void read(SMS sms) { // 讀取 SMS 服務訊息
+        System.out.println(sms.message());
+    }
 }
 
 public class Demo {
     public static void main(String[] args) {
         MyPhone myPhone = new MyPhone();
-        myPhone.read(new Email());
-        myPhone.read(new Line());
+        myPhone.read(new Email()); // 收 email
+        myPhone.read(new Line()); // 收 line
+        myPhone.read(new SMS());  // 收 sms
     }
 }
