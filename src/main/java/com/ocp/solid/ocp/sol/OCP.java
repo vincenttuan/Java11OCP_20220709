93bb494adb6c041139ffde1e4348812e2c559192
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-27 11:10:34 +0800

    java11

---
+++ src/main/java/com/ocp/solid/ocp/sol/OCP.java
@@ -0,0 +1,34 @@
+package com.ocp.solid.ocp.sol;
+
+// 加入一個抽象類別
+abstract class Shape {
+    abstract String draw();
+}
+
+class Rectangle extends Shape {
+    public String draw() { return "畫矩形";};
+}
+
+class Traingle extends Shape {
+    public String draw() { return "畫三角形";};
+}
+
+class Circle extends Shape { // 新功能
+    public String draw() { return "畫圓形";};
+}
+
+class Edit { // 編輯板 (不可改變)
+    String draw(Shape shape) {
+        return shape.draw();
+    }
+}
+
+public class OCP {
+    public static void main(String[] args) {
+        Edit edit = new Edit();
+        edit.draw(new Rectangle());
+        edit.draw(new Traingle());
+        edit.draw(new Circle());
+    }
+}
+
