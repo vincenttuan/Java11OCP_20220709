0c6ae1cda190c5e2709c403f92988a4ab287f852
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-17 14:28:54 +0800

    java11

--- src/main/java/com/ocp/day15_map/MapGroupingBy1.java
+++ src/main/java/com/ocp/day15_map/MapGroupingBy1.java
@@ -1,8 +1,11 @@
 package com.ocp.day15_map;
 
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
+import static java.util.Map.Entry.comparingByValue;
 import static java.util.function.Function.identity;
+import java.util.stream.Collectors;
 import static java.util.stream.Collectors.counting;
 import static java.util.stream.Collectors.groupingBy;
 
@@ -16,5 +19,12 @@ public class MapGroupingBy1 {
                                          //.collect(groupingBy(name -> name, counting()));
                                          .collect(groupingBy(identity(), counting()));
         System.out.println(result);
+        // 排序
+        Map result2 = result.entrySet().stream()
+                //.sorted(comparingByValue())
+                //.sorted(Map.Entry.<String, Long>comparingByValue())
+                .sorted(Map.Entry.<String, Long>comparingByValue().reversed())
+                .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o1, o2) -> o2, LinkedHashMap::new));
+        System.out.println(result2);
     }
 }
