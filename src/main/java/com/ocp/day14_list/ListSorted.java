d5e550b4ff5c1eafda0119e7dfdb70ed8ef8a59d
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-17 11:01:31 +0800

    java11

--- src/main/java/com/ocp/day14_list/ListSorted.java
+++ src/main/java/com/ocp/day14_list/ListSorted.java
@@ -2,7 +2,9 @@ package com.ocp.day14_list;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListSorted {
     public static void main(String[] args) {
@@ -17,7 +19,25 @@ public class ListSorted {
         Collections.sort(scores); // 預設排序原則是自然排序(小->大)
         System.out.println("排序後: " + scores);
         // 進行排序(自訂排序原則)
-        Collections.sort(scores, (o1, o2) -> o2 - o1);
+        //Collections.sort(scores, (o1, o2) -> o2 - o1);
+        Comparator<Integer> c1 = (o1, o2) -> o2 - o1; // (大 -> 小)
+        Comparator<Integer> c2 = (o1, o2) -> o1 - o2; // (小 -> 大)
+        Comparator<Integer> c3 = (a, b) -> a.compareTo(b); // a - b (小 -> 大)
+        Comparator<Integer> c4 = (a, b) -> b.compareTo(a); // b - a (大 -> 小)
+        Comparator<Integer> c5 = c4.reversed(); // 反轉 (大 -> 小) -> (小 -> 大)
+        Collections.sort(scores, c5);
         System.out.println("排序後: " + scores);
+        // 如何將 "集合" 變成 "字串"
+        // List -> String
+        // [80, 90, 100] -> "80 90 100" (元素之間用空白隔開)
+        String str = scores.stream()
+                           //.map(data -> data + "")  // 將每一個元素(data) Integer 轉 String
+                           //.map(data -> String.valueOf(data))  // 將每一個元素(data) Integer 轉 String
+                           .map(String::valueOf)  // 將每一個元素(data) Integer 轉 String
+                           .collect(Collectors.joining(" ")); // 每一個元素間用 " " 隔開
+        System.out.println(str);
+        
+        
+        
     }
 }
