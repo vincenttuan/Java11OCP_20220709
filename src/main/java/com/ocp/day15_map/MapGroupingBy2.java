dc0dad84e37db7d873e16bcb8af2f6bd48da523e
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-17 14:57:31 +0800

    java11

--- src/main/java/com/ocp/day15_map/MapGroupingBy2.java
+++ src/main/java/com/ocp/day15_map/MapGroupingBy2.java
@@ -1,6 +1,10 @@
 package com.ocp.day15_map;
 
 import java.util.List;
+import java.util.Map;
+import static java.util.stream.Collectors.counting;
+import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class MapGroupingBy2 {
     public static void main(String[] args) {
@@ -12,9 +16,18 @@ public class MapGroupingBy2 {
                 new Fruit("papaya", 20, 9.99),
                 new Fruit("apple", 10, 9.99),
                 new Fruit("banana", 10, 19.99),
-                new Fruit("apple", 10, 9.99)
+                new Fruit("apple", 20, 9.99)
         );
         // 希望印出
-        // apple=3, banana=2, orange=1, watermelon=1
+        // papaya=1, orange=1, banana=2, apple=3, watermelon=1
+        Map<String, Long> result = fruits.stream()
+                                         //.collect(groupingBy(fruit -> fruit.getName(), counting()));
+                                         .collect(groupingBy(Fruit::getName, counting()));
+        System.out.println(result);
+        // 希望印出
+        // papaya=20, orange=10, banana=30, apple=40, watermelon=10
+        Map<String, Integer> result2 = fruits.stream()
+                                         .collect(groupingBy(Fruit::getName, summingInt(Fruit::getQty)));
+        System.out.println(result2);
     }
 }
