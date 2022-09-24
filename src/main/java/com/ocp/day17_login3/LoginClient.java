b7301a8e2e9fe0e60a6d43e8e06f141f78752fff
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-24 13:55:23 +0800

    java11

--- src/main/java/com/ocp/day17_login3/LoginClient.java
+++ src/main/java/com/ocp/day17_login3/LoginClient.java
@@ -1,17 +1,29 @@
 package com.ocp.day17_login3;
 
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.Scanner;
 
 public class LoginClient {
+    LoginService loginService = new LoginServiceImpl();
     
     public boolean login() {
-    
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入使用者名稱: ");
+        String name = scanner.next();
+        System.out.print("請輸入使用者密碼: ");
+        String password = scanner.next();
+        boolean result = false;
+        try {
+            result = loginService.login(name, password);
+        } catch (LoginException e) {
+            // 調用 LoginException 的自定方法
+            e.printLoginErrorMessage();
+        }
+        return result;
     }
     
     public static void main(String[] args) {
         LoginClient loginClient = new LoginClient();
-        
+        boolean result = loginClient.login();
+        System.out.println(result);
     }
-    
 }
