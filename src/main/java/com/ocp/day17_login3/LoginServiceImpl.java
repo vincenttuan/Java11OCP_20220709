02e3b4489ed53cb5d94134587a7aecd75c18c344
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-24 14:07:52 +0800

    java11

--- src/main/java/com/ocp/day17_login3/LoginServiceImpl.java
+++ src/main/java/com/ocp/day17_login3/LoginServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ocp.day17_login3;
 
 import com.ocp.day17_login3.exception.LoginException;
+import com.ocp.day17_login3.exception.PasswordNotMatchException;
+import com.ocp.day17_login3.exception.UserNotFoundException;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -17,14 +19,14 @@ public class LoginServiceImpl implements LoginService {
         // 1. 根據 name 來查看是否有此人 ?
         if(users.get(name) == null) { // 無此人
             // 建立一個例外
-            LoginException e1 = new LoginException(name + " 尚未建立帳號!");
+            LoginException e1 = new UserNotFoundException(name + " 尚未建立帳號!");
             throw e1; // 將例外訊息拋出
         }
         // 2. 判斷 password 是否一致?
         boolean result = users.get(name).equals(password);
         if(!result) { // password 不一致
             // 建立一個例外
-            LoginException e2 = new LoginException(name + " 密碼錯誤");
+            LoginException e2 = new PasswordNotMatchException(name + " 密碼錯誤");
             throw e2; // 將例外訊息拋出
         }
         return true; // 登入成功
