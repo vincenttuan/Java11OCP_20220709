ac286ffe7b9a71ef95f03608631a3b911af5871a
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-24 10:39:23 +0800

    java11

---
+++ src/main/java/com/ocp/day16_exception/ExceptionDemo2.java
@@ -0,0 +1,28 @@
+package com.ocp.day16_exception;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class ExceptionDemo2 {
+    
+    public static void main(String[] args) {
+        try {
+            input();
+        } catch (InputMismatchException e) {
+            System.out.println("有錯誤發生");
+            System.out.println(e);
+            e.printStackTrace(System.out); // 詳細錯誤資訊
+            System.out.println("請重新輸入");
+            input();
+        }
+    }
+    
+    public static void input() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("請輸入半徑: ");
+        int r = scanner.nextInt();
+        double area = Math.pow(r, 2) * Math.PI;
+        System.out.printf("圓面積: %,.1f\n", area);
+    }
+    
+}
