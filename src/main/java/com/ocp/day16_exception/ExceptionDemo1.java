c7cbda8f9501ef5a18efaa1c91f5120c2b1d7322
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-24 10:32:47 +0800

    java11

--- src/main/java/com/ocp/day16_exception/ExceptionDemo1.java
+++ src/main/java/com/ocp/day16_exception/ExceptionDemo1.java
@@ -1,5 +1,6 @@
 package com.ocp.day16_exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class ExceptionDemo1 {
@@ -11,9 +12,17 @@ public class ExceptionDemo1 {
     public static void input() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("請輸入半徑: ");
-        int r = scanner.nextInt();
-        double area = Math.pow(r, 2) * Math.PI;
-        System.out.printf("圓面積: %.1f\n", area);
+        try {
+            int r = scanner.nextInt();
+            double area = Math.pow(r, 2) * Math.PI;
+            System.out.printf("圓面積: %,.1f\n", area);
+        } catch (InputMismatchException e) {
+            System.out.println("有錯誤發生");
+            System.out.println(e);
+            System.out.println("請重新輸入");
+            input();
+        }
+        
     }
     
 }
