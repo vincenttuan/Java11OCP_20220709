0d606a7ee3dd0df9f71d8cd05e7f2f29329a609d
MB-teacher <MB-teacher@MB-207-teacher>
2022-09-24 16:08:24 +0800

    java11

--- src/main/java/com/ocp/day17_db/SelectUsers3.java
+++ src/main/java/com/ocp/day17_db/SelectUsers3.java
@@ -10,12 +10,10 @@ public class SelectUsers3 {
     public static void main(String[] args) {
         // SQL 語句
         String sql = "select name, password from users"; // users 資料表 sql 查詢語句
-        
         // 建立資料庫連線(Connection), 資料SQL敘述物件(Statement), 資料回傳物件(ResultSet)
         try(Connection conn = ConnUtil.getConnection();
             Statement stmt = conn.createStatement();
             ResultSet rs = stmt.executeQuery(sql);) {
-            
             while (rs.next()) {                
                 String name = rs.getString("name");
                 String pwd = rs.getString("password");
@@ -24,6 +22,5 @@ public class SelectUsers3 {
         } catch (SQLException e) {
             System.out.println(e);
         }
-        
     }
 }
