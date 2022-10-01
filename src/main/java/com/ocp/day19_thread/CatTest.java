38208cce103c7c0473633f97c47604c6813a7586
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-01 16:23:31 +0800

    java11

--- src/main/java/com/ocp/day19_thread/CatTest.java
+++ src/main/java/com/ocp/day19_thread/CatTest.java
@@ -6,6 +6,13 @@ public class CatTest {
         Car car2 = new Car();
         //car1.driver();
         //car2.driver();
+        // 設定執行緒名稱
+        car1.setName("Toyota");
+        car2.setName("BMW");
+        // 設定權限 (1~10)
+        car1.setPriority(Thread.MIN_PRIORITY); // 1
+        car2.setPriority(Thread.MAX_PRIORITY); // 10
+        // 啟動
         car1.start();
         car2.start();
     }
