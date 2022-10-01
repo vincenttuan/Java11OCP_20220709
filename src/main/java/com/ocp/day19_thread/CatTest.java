aae33d192b9a613f054ee6bada3a802d1486d3f2
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-01 16:07:25 +0800

    java11

---
+++ src/main/java/com/ocp/day19_thread/CatTest.java
@@ -0,0 +1,10 @@
+package com.ocp.day19_thread;
+
+public class CatTest {
+    public static void main(String[] args) {
+        Car car1 = new Car();
+        Car car2 = new Car();
+        car1.driver();
+        car2.driver();
+    }
+}
