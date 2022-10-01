f22f3a9817f38ebcf93c7c2b717e1fd73b5c96c1
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-01 13:53:19 +0800

    java11

---
+++ src/main/java/com/ocp/day18_assertion/Area.java
@@ -0,0 +1,9 @@
+package com.ocp.day18_assertion;
+
+public class Area {
+    // 計算三角形面積
+    public int triangle(int w, int h) {
+        int result = w * h / 2;
+        return result;
+    }
+}
--- src/main/java/com/ocp/day18_assertion/AssertTest.java
+++ src/main/java/com/ocp/day18_assertion/AssertTest.java
@@ -2,7 +2,7 @@ package com.ocp.day18_assertion;
 
 public class AssertTest {
     public static void main(String[] args) {
-        int score = 150;
+        int score = -50;
         
         // assert 檢查
         assert ((score >= 0) && (score <= 100)) : "分數範圍錯誤: " + score;
