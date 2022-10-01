62fc6d19e9ae01bdd272db3e69d1eb734bb8ec78
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-01 16:05:37 +0800

    java11

---
+++ src/main/java/com/ocp/day19_thread/Car.java
@@ -0,0 +1,12 @@
+package com.ocp.day19_thread;
+
+public class Car {
+    
+    public void driver() {
+        String tName = Thread.currentThread().getName();
+        for(int i=1;i<=1000;i++) {
+            System.out.printf("%s 累計開了 %d km\n", tName, i);
+        }
+    }
+    
+}
