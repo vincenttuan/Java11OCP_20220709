47dbbf30615b9eb34bbba0af1e06f8d971f92d37
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-01 13:43:39 +0800

    java11

---
+++ src/main/java/com/ocp/day18_assertion/AssertTest.java
@@ -0,0 +1,12 @@
+package com.ocp.day18_assertion;
+
+public class AssertTest {
+    public static void main(String[] args) {
+        int score = 150;
+        
+        // assert 檢查
+        assert ((score >= 0) && (score <= 100)) : "分數範圍錯誤: " + score;
+        
+        System.out.println("分數: " + score);
+    }
+}
