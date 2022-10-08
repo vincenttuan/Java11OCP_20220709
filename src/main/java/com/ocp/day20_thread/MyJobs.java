35e45285bc37001d3d7fdb955dd6716726f52db8
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-08 09:54:04 +0800

    java11

--- src/main/java/com/ocp/day20_thread/MyJobs.java
+++ src/main/java/com/ocp/day20_thread/MyJobs.java
@@ -22,5 +22,18 @@ class Drink implements Runnable {
 }
 
 public class MyJobs {
-    
+    public static void main(String[] args) {
+        // 1. 建立要執行的工作
+        Program program = new Program();
+        Report report = new Report();
+        Drink drink = new Drink();
+        // 2. 將工作配給執行緒準備執行
+        Thread t1 = new Thread(program);
+        Thread t2 = new Thread(report);
+        Thread t3 = new Thread(drink);
+        // 3. 調用 start() 執行工作任務
+        t1.start();
+        t2.start();
+        t3.start();
+    }
 }
