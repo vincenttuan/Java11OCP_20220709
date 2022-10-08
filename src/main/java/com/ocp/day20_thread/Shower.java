f7b08699566616ab2420d26582e3c3893ec5d7a2
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-08 11:45:02 +0800

    java11

--- src/main/java/com/ocp/day20_thread/Shower.java
+++ src/main/java/com/ocp/day20_thread/Shower.java
@@ -26,5 +26,8 @@ class Worker extends Thread {
 }
 
 public class Shower {
-    
+    public static void main(String[] args) {
+        Father father = new Father();
+        father.start();
+    }
 }
