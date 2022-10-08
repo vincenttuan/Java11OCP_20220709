f21592f627085b7bf6298b784eafbd77449ab033
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-08 15:36:22 +0800

    java11

--- src/test/java/linenotify/LineNotifyDemo4.java
+++ src/test/java/linenotify/LineNotifyDemo4.java
@@ -3,15 +3,17 @@ package linenotify;
 import com.linenotify.service.LineService;
 
 public class LineNotifyDemo4 {
-    static LineService lineService = new LineService();
+    
     static int i;
     public static void main(String[] args) {
+        LineService lineService = new LineService();
         for(i=1;i<=20;i++) {
             Runnable r = () -> {
                 try {
                     int respCode = lineService.sendText("大家好: " + i);
                     System.out.printf("第 %d 筆 回應碼: %d\n", i, respCode);
                 } catch (Exception e) {
+                    e.printStackTrace();
                 }
             };
             Thread t = new Thread(r);
