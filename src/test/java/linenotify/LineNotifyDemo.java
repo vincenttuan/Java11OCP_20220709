324df492dfddc683752e3ab005c1d2f7d8cac229
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-08 14:28:53 +0800

    java11

--- src/test/java/linenotify/LineNotifyDemo.java
+++ src/test/java/linenotify/LineNotifyDemo.java
@@ -1,6 +1,11 @@
 package linenotify;
 
 // 撰寫一個 Line Notify 測試程式
+
+import java.io.DataOutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class LineNotifyDemo {
     public static void main(String[] args) throws Exception {
         // 1-1. 設定要發送的資料訊息
@@ -11,6 +16,23 @@ public class LineNotifyDemo {
         String token = "你自己的 token";
         // 3. 設定 Line Notify 發送中心位置
         String lineNotifyUrl = "https://notify-api.line.me/api/notify";
+        //----------------------------------------------------------------
+        // 4. 建立 URL 物件準備發送
+        URL url = new URL(lineNotifyUrl);
+        // 5. 建立/設定 Http 連線物件
+        HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+        conn.setDoOutput(true);
+        conn.setRequestMethod("POST");
+        conn.setRequestProperty("Authorization", "Bearer " + token);
+        conn.setRequestProperty("Content-Type", "application/x-www-form-urlencoded"); // 可以不用加
+        conn.setUseCaches(false);
+        // 6. 訊息發送
+        try(DataOutputStream dos = new DataOutputStream(conn.getOutputStream())) {
+            dos.write(postData); // 將資料寫入網路串流中
+            dos.flush(); // 提交更新
+        }
+        // 7. 取得回應碼
+        System.out.println("回應碼: " + conn.getResponseCode());
         
     }
 }
