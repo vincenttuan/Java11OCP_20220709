0ed23039b6fb11d3a4370d6e1f19f6d3539434cf
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 11:07:20 +0800

    java11

--- src/main/java/com/ocp/day21_thread/Ball.java
+++ src/main/java/com/ocp/day21_thread/Ball.java
@@ -4,10 +4,18 @@ public class Ball {
     private int num = 10;
     public void take() {
         String tName = Thread.currentThread().getName();
-        for(;num > 0;num--) {
-            int current_num = num; // 取得目前的球號
-            for(int i=0;i<200000000;i++); // 模擬延遲時間
-            System.out.printf("%s 拿了 %d 號球\n", tName, current_num);
+        for(;;) {
+            synchronized (this) {
+                if(num > 0) {
+                    int current_num = num; // 取得目前的球號
+                    for(int i=0;i<200000000;i++); // 模擬延遲時間 1
+                    System.out.printf("%s 拿了 %d 號球\n", tName, current_num);
+                    num--;
+                } else {
+                    break;
+                }
+            }
+            for(int i=0;i<10000;i++); // 模擬延遲時間 2
         }
     }
 }
