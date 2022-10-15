9695f9040f1ee09fd5b4ff82f1e54f51a9622fcf
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 15:19:57 +0800

    java11

--- src/main/java/com/ocp/day22_concurrency/ThreadScheduledDemo.java
+++ src/main/java/com/ocp/day22_concurrency/ThreadScheduledDemo.java
@@ -1,6 +1,9 @@
 package com.ocp.day22_concurrency;
 
 import java.util.Date;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 class PlayJob implements Runnable {
     @Override
@@ -14,6 +17,11 @@ class PlayJob implements Runnable {
 // 重複性排程
 public class ThreadScheduledDemo {
     public static void main(String[] args) {
-        
+        ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+        // 每隔 2 秒鐘重複執行任務
+        int delayTime = 1; // 停 1 秒
+        int peroidTime = 2; // 間隔 2 秒
+        // 1 秒鐘後開始 [執行 MyJob 工作] 間隔 2 秒 [執行 MyJob 工作] 間隔 2 秒 [執行 MyJob 工作] ...
+        service.scheduleWithFixedDelay(new PlayJob(), delayTime, peroidTime, TimeUnit.SECONDS);
     }
 }
