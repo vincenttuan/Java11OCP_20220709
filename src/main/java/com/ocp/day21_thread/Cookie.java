789eac5271770c783d745e7032ca8664a86c6b85
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 12:13:26 +0800

    java11

--- src/main/java/com/ocp/day21_thread/Cookie.java
+++ src/main/java/com/ocp/day21_thread/Cookie.java
@@ -29,7 +29,7 @@ class Put implements Runnable {
 public class Cookie {
     private boolean empty = true; // 放餅乾的盤子
     public synchronized void eat(int i) { // 吃餅乾程序
-        if(empty) {
+        while(empty) {
             try {
                 wait(); // 等待
             } catch (Exception e) {
@@ -38,11 +38,11 @@ public class Cookie {
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 吃了第 %2d 塊餅乾\n", tName, i);
         empty = true; // 餅乾吃光了
-        notify(); // 呼叫主人放餅乾
+        notifyAll(); // 呼叫主人放餅乾
     }
     
     public synchronized void put(int i) { // 放餅乾程序
-        if(!empty) {
+        while(!empty) {
             try {
                 wait(); // 等待
             } catch (Exception e) {
@@ -51,7 +51,7 @@ public class Cookie {
         String tName = Thread.currentThread().getName();
         System.out.printf("%s 放了第 %2d 塊餅乾, ", tName, i);
         empty = false; // 盤子內有餅乾
-        notify(); // 呼叫小狗吃餅乾
+        notifyAll(); // 呼叫小狗吃餅乾
     }
     
 }
