6a11dd03bf977c3c21efb574d887a0978f3baf72
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 14:30:00 +0800

    java11

---
+++ src/main/java/com/ocp/day22_concurrency/ThreadPoolDemo1.java
@@ -0,0 +1,30 @@
+package com.ocp.day22_concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+class Job implements Runnable {
+    @Override
+    public void run() {
+        String tName = Thread.currentThread().getName();
+        System.out.printf("%s 任務進行中...\n", tName);
+        for(int i=0;i<=10000;i++);
+        System.out.printf("%s 任務進行完畢\n", tName);
+    }
+}
+
+public class ThreadPoolDemo1 {
+    public static void main(String[] args) {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(new Job());
+        service.submit(new Job());
+        service.submit(new Job());
+        service.submit(new Job());
+        service.submit(new Job());
+        
+        // 停止服務
+        service.shutdown(); // 平滑結束(會等到所有執行緒的工作都做完才會結束)
+        //service.shutdownNow(); // 強制結束(不會等到所有執行緒的工作做完, 強制結束)
+       
+    }
+}
