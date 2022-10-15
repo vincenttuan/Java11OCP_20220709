3b96a4d7959a1281da4646c6525e3e6f6bf79f6f
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 09:52:38 +0800

    java11

--- src/main/java/com/ocp/day21_thread/Account.java
+++ src/main/java/com/ocp/day21_thread/Account.java
@@ -9,11 +9,11 @@ public class Account {
 
     public void withdraw(String amount) {
         String tName = Thread.currentThread().getName();
-        System.out.printf("%s 得到的帳戶餘額: $%d\n", tName, balance);
+        System.out.printf("%s 得到的帳戶餘額(1): $%d\n", tName, balance);
         
         for(int i=0;i<=2100000000;i++); // 模擬延遲
         
-        System.out.printf("%s 得到的帳戶餘額: $%d\n", tName, balance);
+        System.out.printf("%s 得到的帳戶餘額(2): $%d\n", tName, balance);
     }
     
 }
