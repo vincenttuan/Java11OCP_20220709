14a25cc79c99eaf2e6034d98b53556ace873adfe
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-15 14:38:16 +0800

    java11

---
+++ src/main/java/com/ocp/day22_concurrency/JobMain.java
@@ -0,0 +1,25 @@
+package com.ocp.day22_concurrency;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class JobMain {
+    public static void main(String[] args) throws Exception {
+        ExecutorService service = Executors.newCachedThreadPool();
+        service.submit(new LongJob());
+        service.submit(new Job());
+        service.submit(new Job());
+        service.submit(new LongJob());
+        service.submit(new Job());
+        
+        service.shutdown();
+        //service.shutdownNow();
+        
+        // 每一秒去偵測執行緒池是否關閉 ?
+        while (!service.awaitTermination(1, TimeUnit.SECONDS)) {
+            System.out.println("執行緒池尚未關閉");
+        }
+        System.out.println("執行緒已關閉");
+    }
+}
