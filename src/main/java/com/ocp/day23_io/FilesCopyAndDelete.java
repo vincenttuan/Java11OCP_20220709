2346fc43cb4c9e6a0df53c5a3b445a6bbc6a5111
MB-teacher <MB-teacher@MB-207-teacher>
2022-10-22 14:52:27 +0800

    java11

--- src/main/java/com/ocp/day23_io/FilesCopyAndDelete.java
+++ src/main/java/com/ocp/day23_io/FilesCopyAndDelete.java
@@ -1,7 +1,24 @@
 package com.ocp.day23_io;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+
 public class FilesCopyAndDelete {
     public static void main(String[] args) {
+        Path sourcePath = Paths.get("src/main/java/com/ocp/day23_io/bar/A.txt");
+        Path distPath = Paths.get("src/main/java/com/ocp/day23_io/foo/A.txt");
+        try {
+            Files.copy(sourcePath, distPath, StandardCopyOption.REPLACE_EXISTING); // 複製
+            System.out.println("複製完畢");
+            // 刪除來源端
+            Files.delete(sourcePath);
+            System.out.println("來源端檔案刪除");
+        } catch (IOException e) {
+            System.out.println(e);
+        }
         
     }
 }
